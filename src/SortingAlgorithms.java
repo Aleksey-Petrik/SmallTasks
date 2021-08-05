1384bccdc86205ba9fe0e26d328e2bd57c7da6c7
Aleksey Petrik <alexey.petric@gmail.com>
2021-08-05 22:20:40 +1000

    class SortingAlgorithms methods algorithms sorting. Added bubbleMethod and insertMethod.

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class SortingAlgorithms {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(bubbleMethod(new int[]{4, 3, 7, 1, 0, 2})));
+        System.out.println(Arrays.toString(insertMethod(new int[]{4, 3, 7, 1, 0, 2})));
+    }
+
+    /*
+        Сортировка методом "Пузырька"
+     */
+    public static int[] bubbleMethod(int[] array) {
+        boolean turn = true;
+        while (turn) {
+            turn = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    turn = true;
+                }
+            }
+        }
+        return array;
+    }
+
+    /*
+        Сортировка методом "Вставки"
+     */
+    public static int[] insertMethod(int[] array){
+        for (int i = 1; i < array.length; i++){
+            for(int j = i; j > 0 && array[j - 1] > array[j]; j--){
+                swap(array, j - 1, j);
+            }
+        }
+        return array;
+    }
+
+    /*
+        Метод замены елементов
+     */
+    private static void swap(int[] array, int currentIndex, int swapIndex) {
+        int buf = array[currentIndex];
+        array[currentIndex] = array[swapIndex];
+        array[swapIndex] = buf;
+    }
+}
