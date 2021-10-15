162e6731a1763184d7c446a4f4be02f3ffab02e9
Aleksey Petrik <alexey.petric@gmail.com>
2021-10-15 22:56:17 +1000

    DataSet plus parametrization.

---
+++ src/main/java/devstudy/java_se_http_server/block1/task3/DataSet.java
@@ -0,0 +1,31 @@
+package devstudy.java_se_http_server.block1.task3;
+
+public abstract class DataSet<T> {
+  protected int size;
+
+  public DataSet() {
+    size = 0;
+  }
+
+  public final int size() {
+    return size;
+  }
+
+  public void clear() {
+    size = 0;
+  }
+
+  public abstract void add(T element);
+
+  public abstract T get(int index);
+
+  public abstract T remove(int index);
+
+  public abstract T[] toArray();
+
+  protected void checkIndex(int index) {
+    if (index >= size || index < 0) {
+      throw new IllegalArgumentException();
+    }
+  }
+}
