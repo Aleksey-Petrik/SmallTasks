640e9abd1180b3946c170803dae262b989636c0d
Aleksey Petrik <alexey.petric@gmail.com>
2021-11-30 20:41:39 +1000

    practice Iterator.

---
+++ src/main/java/practicejava/iterator/MainIterator.java
@@ -0,0 +1,24 @@
+package practicejava.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MainIterator {
+    public static void main(String[] args) {
+        Consumer<String> consumerPrint = System.out::println;
+
+        List<String> list = new ArrayList<>();
+        list.add("Alex");
+        list.add("Mary");
+        list.add("Bob");
+
+        Iterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        iterator.forEachRemaining(consumerPrint);
+    }
+
+}
